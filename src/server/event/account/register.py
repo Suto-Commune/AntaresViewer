3e970a79971d7ca0ac905264718edbf65a347dfe
hsn <hsn1919810@gmail.com>
2024-02-20 09:07:13 +0800

    更新密码哈希算法&修复一些问题

--- src/server/event/account/register.py
+++ src/server/event/account/register.py
@@ -1,9 +1,12 @@
-import fastapi
-import src.function.format.avi as AVI
-import hashlib
+import os
 from typing import Union
+
+import fastapi
+
 import src.data.datablock as datablock
+import src.function.format.avi as AVI
 import src.function.interesting.random_string as random_string
+from src.utils.crypto import password_hash
 
 Name = AVI.Name()
 default_url = Name.default_url
@@ -12,27 +15,25 @@ router = fastapi.APIRouter()
 
 @router.get(default_url)
 async def register(username: str, password: str, sid: Union[str, None] = None):
-    sha256_hash = hashlib.sha256()
-    sha256_hash.update(password.encode('utf-8'))
-    sha256_hex = sha256_hash.hexdigest()
+    salt = os.urandom(16)
+    _hash = password_hash(password=password, n=16384, r=8, p=1, maxmem=0, dklen=64)
     user = {
         "username": username,
-        "password": sha256_hex
+        "password": _hash
     }
     DB = datablock.DataBlock("", "users")
     DB.create()
     info = DB.diiiict()
     if sid is None:
         sid = random_string.random_string()
-
-    if info is None:
+    if info:
+        user["id"] = len(info["users"])
+    else:
         info = {"users": []}
         user["id"] = 0
-    else:
-        info=info
-        user["id"] = len(info["users"])
+
     user["sid"] = sid
     info["users"].append(user)
     DB.update(info)
-    print(user)
+
     return {"Hello,AntaresViewer"}
