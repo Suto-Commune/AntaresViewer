1f395699e0130c2aef5ef1295735416a7bff29c2
hsn <hsn1919810@gmail.com>
2024-02-24 14:24:23 +0800

    修复配置读取

--- src/function/format/avi.py
+++ src/function/format/avi.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+
 from src.toml_config import config
 
 
@@ -15,27 +16,27 @@ class Name:
 
 
 def is_valid_email(email):
-    regex=""
-    if config.server.strict:
+    regex = ""
+    if config.server.strict_email:
         # regex for email address
         regex = r'^[\w.%+-]+@(gmail\.com|outlook\.com|yahoo\.com|msn\.com|hotmail\.com|aol\.com|ask\.com|live\.com|qq\.com|0355\.net' \
-                      r'|163\.com|163\.net|263\.net|3721\.net|yeah\.net|googlemail\.com|mail\.com|aim\.com|walla\.com|inbox' \
-                      r'\.com|126\.com|sina\.com|21cn\.com|sohu\.com|yahoo\.com\.zh_CN|tom\.com|etang\.com|eyou\.com|56\.com|' \
-                      r'x' \
-                      r'\.zh_CN|chinaren\.com|sogou\.com|citiz\.com|hongkong\.com|ctimail\.com|hknet\.com|netvigator\.com|' \
-                      r'mail' \
-                      r'\.hk\.com|swe\.com\.hk|ITCCOLP\.COM\.HK|BIZNETVIGATOR\.COM|SEED\.NET\.TW|TOPMARKEPLG\.COM\.TW|PCHOME' \
-                      r'\.COM\.TW|hinet\.net\.tw|cyber\.net\.pk|omantel\.net\.om|libero\.it|webmail\.co\.za|xtra\.co\.nz' \
-                      r'|pacific\.net\.sg|FASTMAIL\.FM|emirates\.net\.ae|eim\.ae|net\.sy|scs-net\.org|mail\.sy|ttnet\.net\.tr' \
-                      r'|superonline\.com|yemen\.net\.ye|y\.net\.ye|cytanet\.com\.cy|aol\.com|netzero\.net|twcny\.rr\.com' \
-                      r'|comcast\.net|warwick\.net|cs\.com|verizon\.net|bigpond\.com|otenet\.gr|vsnl\.com|wilnetonline\.net' \
-                      r'|cal3\.vsnl\.net\.in|rediffmail\.com|sancharnet\.in|NDF\.VSNL\.NET\.IN|DEL3\.VSNL\.NET\.IN|xtra\.co' \
-                      r'\.nz|yandex\.ru|t-online\.de|NETVISION\.NET\.IL|BIGPOND\.NET\.AU|MAIL\.RU|EV|ADSL\.LOXINFO\.COM|SCS' \
-                      r'-NET\.ORG|EMIRATES\.NET\.AE|QUALITYNET\.NET|ZAHAV\.NET\.IL|netvision\.net\.il|xx\.org\.il|hn\.vnn\.vn' \
-                      r'|hcm\.fpt\.vn|hcm\.vnn\.vn|candel\.co\.jp|zamnet\.zm|amet\.com\.ar|infovia\.com\.ar|mt\.net\.mk' \
-                      r'|sotelgui\.net\.gn|prodigy\.net\.mx|citechco\.net|xxx\.meh\.es|terra\.es|wannado\.fr|mindspring\.com' \
-                      r'|excite\.com|africaonline\.co\.zw|samara\.co\.zw|zol\.co\.zw|mweb\.co\.zw|aviso\.ci|africaonline\.co' \
-                      r'\.ci|afnet\.net|mti\.gov\.na|namibnet\.com|iway\.na|be-local\.com|infoclub\.com\.np)$'
-    elif not config.server.strict:
+                r'|163\.com|163\.net|263\.net|3721\.net|yeah\.net|googlemail\.com|mail\.com|aim\.com|walla\.com|inbox' \
+                r'\.com|126\.com|sina\.com|21cn\.com|sohu\.com|yahoo\.com\.zh_CN|tom\.com|etang\.com|eyou\.com|56\.com|' \
+                r'x' \
+                r'\.zh_CN|chinaren\.com|sogou\.com|citiz\.com|hongkong\.com|ctimail\.com|hknet\.com|netvigator\.com|' \
+                r'mail' \
+                r'\.hk\.com|swe\.com\.hk|ITCCOLP\.COM\.HK|BIZNETVIGATOR\.COM|SEED\.NET\.TW|TOPMARKEPLG\.COM\.TW|PCHOME' \
+                r'\.COM\.TW|hinet\.net\.tw|cyber\.net\.pk|omantel\.net\.om|libero\.it|webmail\.co\.za|xtra\.co\.nz' \
+                r'|pacific\.net\.sg|FASTMAIL\.FM|emirates\.net\.ae|eim\.ae|net\.sy|scs-net\.org|mail\.sy|ttnet\.net\.tr' \
+                r'|superonline\.com|yemen\.net\.ye|y\.net\.ye|cytanet\.com\.cy|aol\.com|netzero\.net|twcny\.rr\.com' \
+                r'|comcast\.net|warwick\.net|cs\.com|verizon\.net|bigpond\.com|otenet\.gr|vsnl\.com|wilnetonline\.net' \
+                r'|cal3\.vsnl\.net\.in|rediffmail\.com|sancharnet\.in|NDF\.VSNL\.NET\.IN|DEL3\.VSNL\.NET\.IN|xtra\.co' \
+                r'\.nz|yandex\.ru|t-online\.de|NETVISION\.NET\.IL|BIGPOND\.NET\.AU|MAIL\.RU|EV|ADSL\.LOXINFO\.COM|SCS' \
+                r'-NET\.ORG|EMIRATES\.NET\.AE|QUALITYNET\.NET|ZAHAV\.NET\.IL|netvision\.net\.il|xx\.org\.il|hn\.vnn\.vn' \
+                r'|hcm\.fpt\.vn|hcm\.vnn\.vn|candel\.co\.jp|zamnet\.zm|amet\.com\.ar|infovia\.com\.ar|mt\.net\.mk' \
+                r'|sotelgui\.net\.gn|prodigy\.net\.mx|citechco\.net|xxx\.meh\.es|terra\.es|wannado\.fr|mindspring\.com' \
+                r'|excite\.com|africaonline\.co\.zw|samara\.co\.zw|zol\.co\.zw|mweb\.co\.zw|aviso\.ci|africaonline\.co' \
+                r'\.ci|afnet\.net|mti\.gov\.na|namibnet\.com|iway\.na|be-local\.com|infoclub\.com\.np)$'
+    else:
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
     return re.fullmatch(regex, email) is not None
